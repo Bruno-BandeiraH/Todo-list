9879eed00ee0253bf314087cac2daf09bfa2af40
Bruno Bandeira <brunohenrique0665@gmail.com>
2023-05-23 11:38:46 -0300

    adicionando função de remover e editar tarefas

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
+let oldInputValue; // Vai guardar o nome antigo de uma tarefa;
+
 /*Funções*/
 const saveTodo = (text) => {
   const todo = document.createElement("div"); // cria uma tag div no document
@@ -33,7 +35,25 @@ const saveTodo = (text) => {
   todoList.appendChild(todo); //Adiciona a tarefa na lista de tarefas
   todoInput.value = "";
   todoInput.focus();
-}
+};
+
+const toggleForms = () => {
+  editForm.classList.toggle("hide");
+  todoForm.classList.toggle("hide");
+  todoList.classList.toggle("hide");
+};
+
+const updateTodo = (text) => {
+  const todos = document.querySelectorAll(".todo");
+
+  todos.forEach((todo) => {
+    let todoTitle = todo.querySelector("h3");
+
+    if(todoTitle.innerText === oldInputValue) {
+      todoTitle.innerText = text;
+    }
+  });
+};
 
 /*Eventos*/
 todoForm.addEventListener("submit", (e) => {
@@ -42,7 +62,52 @@ todoForm.addEventListener("submit", (e) => {
   const inputValue = todoInput.value;
 
   if(inputValue) {
-    saveTodo(inputValue)
+    saveTodo(inputValue);
     //save todo
   }
-})
+});
+
+//evento global para saber qual elemento foi clicado
+document.addEventListener("click", (e) => {
+  const targetElement = e.target;
+  const parentElement = targetElement.closest("div"); // seleciona o elemento pai div mais próximo
+  let todoTitle; // Essa variavel vai guardar o título das tarefas para fazer alterações nelas
+
+  // Guarda o título da tarefa se o elemento tiver uma tag h3 dentro dele
+  if(parentElement && parentElement.querySelector("h3")) {
+    todoTitle = parentElement.querySelector("h3").innerText; 
+  }
+
+  if(targetElement.classList.contains("finish-todo")) {
+    parentElement.classList.toggle("done"); // Adiciona/remove a classe e pelo CSS definido marca a tarefa como concluída
+  }
+
+  if(targetElement.classList.contains("remove-todo")) {
+    parentElement.remove(); // Exclui a tarefa
+  }
+
+  if(targetElement.classList.contains("edit-todo")) {
+    toggleForms();
+
+    editInput.value = todoTitle;
+    oldInputValue = todoTitle;
+  }
+});
+
+cancelEditBtn.addEventListener("click", (e) => {
+  e.preventDefault(); // Cancela o envio do formulário
+
+  toggleForms(); // Chama a função para alternar os itens que aparecem na tela
+});
+
+editForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const editInputValue = editInput.value;
+
+  if(editInputValue) {
+    updateTodo(editInputValue);
+  }
+
+  toggleForms();
+});
